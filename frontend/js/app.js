62dd8c3f3961aaaa4658946e1a2b25368b983e44
Irene Fuentes <fuentes.acedo1.@gmail.com>
2016-06-21 17:02:04 +0100

    built edit and update user form functions

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  getAllUsers()
-  
-});
+  getAllUsers();
+  $("form#register").on("submit", createUser);
+  $('body').on('click', '.edit', editUser);
 
 //
   function getAllUsers() {
@@ -25,3 +25,60 @@ function getElements(user, div){
   $(div).append(person);
 
 }
+
+//===================================
+
+//EDIT USER
+
+function editUser(){
+  $.ajax({
+    method: 'get',
+    url: 'http://localhost:3000/users/:id'+$(this).data().id
+  }).done(function(user){
+    $("input#edit-name").val(user.name),
+    $("input#edit-email").val(user.email),
+    $("input#edit-password").val(user.password),
+    $("input#edit-passwordConfirmation").val(user.passwordConfirmation)
+    $('form#editProfile').slideDown()
+  });
+  $('#editProfile').on('submit', updateUser);
+}
+
+var updateUser = function(){
+  event.preventDefault();
+  var user = {
+    user:{
+      name: $("input#edit-name").val(),
+      email: $("input#edit-email").val(),
+      password: $("input#edit-password").val(),
+      passwordConfirmation: $("input#edit-passwordConfirmation").val()
+    }
+  };
+  $.ajax({
+    method: 'patch',
+    url: 'http://localhost:3000/users/:id'+$(this).data().id,
+    data: user
+  }).done(function(data){
+    // not ideal
+    // location.reload();
+  });
+}
+
+// leave this all the function have to be above the line 68
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
