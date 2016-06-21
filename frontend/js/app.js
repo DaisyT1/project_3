23a4e6caa57182f97b7e14087114bc0a61ace8df
Daisy Tucker <daisyatucker@gmail.com>
2016-06-21 13:26:17 +0100

    Users index awaiting tokens to display

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-
+  getAllUsers()
   
-}
+});
 
+//
+  function getAllUsers() {
+  $.get("http://localhost:3000/api/users" , function(users) {
+
+    $(users).each(function(index, user){
+      getElements(user , "#index")
+      console.log(user.name)
+    })
+
+  })
+};
+
+function getElements(user, div){
+
+  var person = 
+  "<p>" + user.name  + "</p>" +
+  "<p>" + user.email  + "</p>" 
+
+
+  $(div).append(person);
+
+}
