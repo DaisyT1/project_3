0f0677e7e7a8dc4289ea885c1e5074c52c738a75
Daisy Tucker <daisyatucker@gmail.com>
2016-06-24 11:21:14 +0100

    Map style change

--- api/public/js/app.js
+++ api/public/js/app.js
@@ -362,7 +362,8 @@ function navbarToggle() {
   var mapOptions = {
     zoom:12,
     center: new google.maps.LatLng(51.506178,-0.088369),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
   }
 
   var map = new google.maps.Map(canvas , mapOptions);
